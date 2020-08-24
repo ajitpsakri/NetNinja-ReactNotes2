b2182b422bd3c7c08b99b51f31aaf275be338446
Ajit <smartsakri@gmail.com>
2020-08-24 12:33:30 +0530

    Ternary-operator

--- src/components/Ninjas.js
+++ src/components/Ninjas.js
@@ -2,17 +2,13 @@ import React from "react";
 
 const Ninjas = ({ ninjas }) => {
   const ninjaList = ninjas.map((ninja) => {
-    if (ninja.age > 20) {
-      return (
-        <div className="ninja" key={ninja.id}>
-          <div>name:{ninja.name}</div>
-          <div>Age:{ninja.age}</div>
-          <div>Belt:{ninja.belt}</div>
-        </div>
-      );
-    } else {
-      return null;
-    }
+    return ninja.age > 20 ? (
+      <div className="ninja" key={ninja.id}>
+        <div>name:{ninja.name}</div>
+        <div>Age:{ninja.age}</div>
+        <div>Belt:{ninja.belt}</div>
+      </div>
+    ) : null;
   });
   return <div className="ninja-list">{ninjaList}</div>;
 };
