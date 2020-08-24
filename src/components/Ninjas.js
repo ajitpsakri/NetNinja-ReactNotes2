a6a17f0df6d93b0f611151fc4f48e1f247fd8ae5
Ajit <smartsakri@gmail.com>
2020-08-24 11:30:47 +0530

    stateless-component-notes

--- src/components/Ninjas.js
+++ src/components/Ninjas.js
@@ -1,21 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Ninjas extends Component {
-  render() {
-    //destructoring of props
-    const { ninjas } = this.props;
-    //creating a new array to render in the dom
-    const ninjaList = ninjas.map((ninja) => {
-      return (
-        <div className="ninja" key={ninja.id}>
-          <div>name:{ninja.name}</div>
-          <div>Age:{ninja.age}</div>
-          <div>Belt:{ninja.belt}</div>
-        </div>
-      );
-    });
-    return <div className="ninja-list">{ninjaList}</div>;
-  }
-}
+//lets destrucre in the parameter
+const Ninjas = ({ ninjas }) => {
+  //creating a new array to render in the dom
+  const ninjaList = ninjas.map((ninja) => {
+    return (
+      <div className="ninja" key={ninja.id}>
+        <div>name:{ninja.name}</div>
+        <div>Age:{ninja.age}</div>
+        <div>Belt:{ninja.belt}</div>
+      </div>
+    );
+  });
+  return <div className="ninja-list">{ninjaList}</div>;
+};
 
 export default Ninjas;
