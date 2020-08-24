7b367fa5b48c7692ff4c90dd05b68fa9d35862ef
Ajit <smartsakri@gmail.com>
2020-08-24 12:30:56 +0530

    if-else

--- src/components/Ninjas.js
+++ src/components/Ninjas.js
@@ -1,16 +1,18 @@
 import React from "react";
 
-//lets destrucre in the parameter
 const Ninjas = ({ ninjas }) => {
-  //creating a new array to render in the dom
   const ninjaList = ninjas.map((ninja) => {
-    return (
-      <div className="ninja" key={ninja.id}>
-        <div>name:{ninja.name}</div>
-        <div>Age:{ninja.age}</div>
-        <div>Belt:{ninja.belt}</div>
-      </div>
-    );
+    if (ninja.age > 20) {
+      return (
+        <div className="ninja" key={ninja.id}>
+          <div>name:{ninja.name}</div>
+          <div>Age:{ninja.age}</div>
+          <div>Belt:{ninja.belt}</div>
+        </div>
+      );
+    } else {
+      return null;
+    }
   });
   return <div className="ninja-list">{ninjaList}</div>;
 };
